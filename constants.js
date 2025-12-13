ac7858c11a14f7264f96a450e977906604187346
media-frameworks <mikehallvideo@gmail.com>
2025-12-12 20:52:09 -0500

    loading data server now

--- constants.js
+++ constants.js
@@ -1,3 +1,15 @@
 export const FRACTO_APP_PORT = 3000;
 export const FRACTO_SERVER_PORT = 3001;
 export const FRACTO_DATA_PORT = 3002;
+
+export const SERVICE_NAME_DATA = 'fracto-data-server'
+
+export const ALL_SERVICE_NAMES = [
+   SERVICE_NAME_DATA,
+]
+
+export const EXEC_SYNC_OPTIONS = {
+   stdio: 'inherit',
+   stderr: 'inherit',
+   stdout: 'inherit',
+}
