efc8fddf397667fee4232e7326aed19a80afe14e
media-frameworks <mikehallvideo@gmail.com>
2026-04-05 20:11:08 -0400

    performance

--- sdk/FractoHyperCalc.js
+++ sdk/FractoHyperCalc.js
@@ -1,7 +1,7 @@
 import Complex from "./math/Complex.js";
 
 const MAX_ORBITAL_SIZE = 5000
-const MIN_ITERATION = 1000000
+const MIN_ITERATION = 10000000
 
 export class FractoHyperCalc {
 
@@ -21,8 +21,8 @@ export class FractoHyperCalc {
       const iteration_factor = (MIN_ITERATION * level / 10) + MAX_ORBITAL_SIZE
       const max_iteration = Math.round(iteration_factor / MAX_ORBITAL_SIZE) * MAX_ORBITAL_SIZE
       for (; iteration < max_iteration; iteration++) {
-         Q_y = 2 * Q_x * Q_y - P_y - Q_y_squared;
-         Q_x = Q_x_squared - P_x;
+         Q_y = 2 * Q_x * Q_y + P_y - Q_y_squared;
+         Q_x = Q_x_squared + P_x;
          Q_x_squared = Q_x * Q_x
          Q_y_squared = Q_y * Q_y
          if (Q_x_squared + Q_y_squared > 100) {
@@ -39,8 +39,8 @@ export class FractoHyperCalc {
             if (Q_x === first_pos.x && Q_y === first_pos.y) {
                const orbital_points = []
                for (let i = 0; i < orbital + 1; i++) {
-                  Q_y = 2 * Q_x * Q_y - P_y - Q_y_squared;
-                  Q_x = Q_x_squared - P_x;
+                  Q_y = 2 * Q_x * Q_y + P_y - Q_y_squared;
+                  Q_x = Q_x_squared + P_x;
                   Q_x_squared = Q_x * Q_x
                   Q_y_squared = Q_y * Q_y
                   orbital_points.push({
@@ -55,21 +55,21 @@ export class FractoHyperCalc {
                };
             }
          }
-
-         if (iteration > max_iteration - MAX_ORBITAL_SIZE) {
-            estimated = true
-            const difference = new Complex(Q_x - first_pos.x, Q_y - first_pos.y)
-            const mag_difference = difference.magnitude()
-            if (mag_difference < least_magnitude) {
-               least_magnitude = mag_difference
-               best_orbital = orbital
-            }
-         }
+         //
+         // if (iteration > max_iteration - MAX_ORBITAL_SIZE) {
+         //    estimated = true
+         //    const difference = new Complex(Q_x - first_pos.x, Q_y - first_pos.y)
+         //    const mag_difference = difference.magnitude()
+         //    if (mag_difference < least_magnitude) {
+         //       least_magnitude = mag_difference
+         //       best_orbital = orbital
+         //    }
+         // }
       }
       const orbital_points = []
       for (let i = 0; i < best_orbital + 1; i++) {
-         Q_y = 2 * Q_x * Q_y - P_y - Q_y_squared;
-         Q_x = Q_x_squared - P_x;
+         Q_y = 2 * Q_x * Q_y + P_y - Q_y_squared;
+         Q_x = Q_x_squared + P_x;
          Q_x_squared = Q_x * Q_x
          Q_y_squared = Q_y * Q_y
          orbital_points.push({
